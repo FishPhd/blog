3105e7f016752d0d888c66c2de112231ea8174c9
Sam Fish <samuel.fish@live.com>
2024-04-28 15:03:04 -0700

    Complete framer animations

--- components/DivisionGroupsDemo/DivisionGroupsDemo.tsx
+++ components/DivisionGroupsDemo/DivisionGroupsDemo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useId } from "react";
 import { range } from "@/utils";
 import Equation from "./Equation";
 import Card from "../Card/Card";
@@ -7,12 +7,24 @@ import SliderControl from "../SliderControl/SliderControl";
 import clsx from "clsx";
 import Spinner from "../Spinner/Spinner";
 import dynamic from "next/dynamic";
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
 
 interface DivisionGroupsDemoProps {
   numOfItems?: number;
   initialNumOfGroups?: number;
   includeRemainderArea?: boolean;
   className?: string;
+}
+
+function chunkArray<T>(originalArray: T[], size: number): T[][] {
+  const array = [...originalArray]; // Create a copy to preserve the original array
+  const chunks: T[][] = [];
+
+  while (array.length > 0) {
+    chunks.push(array.splice(0, size));
+  }
+
+  return chunks;
 }
 
 export const DivisionGroupsDemoLazy = dynamic(
@@ -28,8 +40,10 @@ function DivisionGroupsDemo({
   includeRemainderArea = false,
   className,
 }: DivisionGroupsDemoProps) {
+  const id = useId();
   const [numOfGroups, setNumOfGroups] = React.useState(initialNumOfGroups);
   const numOfItemsPerGroup = Math.floor(numOfItems / numOfGroups);
+
   const remainder = includeRemainderArea ? numOfItems % numOfGroups : null;
 
   // Dynamic grid structure adjustment based on the number of groups
@@ -51,28 +65,35 @@ function DivisionGroupsDemo({
           min={1}
           max={4}
           value={numOfGroups}
-          onChange={(ev) => setNumOfGroups(Number(ev.target.value))}
+          onChange={(ev) => setNumOfGroups(ev.target.valueAsNumber)}
         />
       </header>
 
       <div className="relative w-full max-w-3xl">
-        <div
-          className={`grid ${gridClasses} gap-4 h-75 border-2 border-gray-500 p-4`}
-        >
-          {range(numOfGroups).map((groupIndex) => (
-            <div
-              key={groupIndex}
-              className="flex gap-2 flex-wrap items-center justify-center align-content-center p-2 border border-gray-300"
-            >
-              {range(numOfItemsPerGroup).map((index) => (
-                <div
-                  key={index}
-                  className="relative z-20 w-8 h-8 bg-green-300 rounded-full flex justify-center items-center font-bold"
-                ></div>
-              ))}
-            </div>
-          ))}
-        </div>
+        <LayoutGroup>
+          <div
+            className={`grid ${gridClasses} gap-4 h-75 border-2 border-gray-500 p-4`}
+          >
+            {range(numOfGroups).map((items, groupIndex) => (
+              <div
+                key={groupIndex.toString()}
+                className="flex gap-2 flex-wrap items-center justify-center align-content-center p-2 border border-gray-300"
+              >
+                {range(numOfItemsPerGroup).map((index) => {
+                  const totalInPreviousGroups = groupIndex * numOfItemsPerGroup;
+                  const layoutId = `${id}-${index + totalInPreviousGroups}`;
+                  return (
+                    <motion.div
+                      key={layoutId}
+                      layoutId={layoutId}
+                      className="relative z-20 w-8 h-8 bg-green-300 rounded-full flex justify-center items-center font-bold"
+                    ></motion.div>
+                  );
+                })}
+              </div>
+            ))}
+          </div>
+        </LayoutGroup>
       </div>
 
       {includeRemainderArea && (
